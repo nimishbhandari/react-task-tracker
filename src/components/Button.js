6a8f228d68d4dee07c7ac91bfdb4fe09033c1754
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-02-16 13:54:52 +0530

    header and button done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = () => {
+const Button = ({ color, text, onClick }) => {
   return (
     <div>
-      <button className="btn">Add</button>
+      <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
+        {text}
+      </button>
     </div>
   );
+};
+
+Button.defaultProps = {
+  color: "blue",
+  text: "hello",
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,14 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click1");
+  };
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button />
+      <Button onClick={onClick} color="green" text="Add" />
     </header>
   );
 };
