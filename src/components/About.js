9c79e1c0416544124dfe0d7e0303e485e2a3c09a
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-02-16 23:52:44 +0530

    final

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function About() {
+  return (
+    <div>
+      <h1>Version:1.1.1</h1>
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+}
+
+export default About;
